b5224b886fc013b2a3268ca6d1d9c07ff0d57641
Hrushikesh1430 <61748949+Hrushikesh1430@users.noreply.github.com>
2020-09-07 14:34:13 +0530

    My first commit
    maja

---
+++ js/portfolio.js
@@ -0,0 +1,14 @@
+AOS.init();{
+    
+}
+
+var typed = new Typed('.animate', {
+    strings: [
+        "Engineer",
+        "Content writer",
+        "Musician"
+    ],
+    typespeed:50,
+    backspeed:50,
+    loop:true
+});
